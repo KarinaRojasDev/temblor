ac191347aa876c7781060956e35a017d22cf839a
Karina Rojas <karinacodecompetent@gmail.com>
2026-03-23 19:30:43 +0100

    feat: añadir botón a favoritos en popup del mapa 1

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
+let firebaseConfig = {
+  apiKey: "AIzaSyAublgEooD1AsPPpurfrf61UhRJkjpOkvw",
+  authDomain: "temblor-db.firebaseapp.com",
+  projectId: "temblor-db",
+  storageBucket: "temblor-db.firebasestorage.app",
+  messagingSenderId: "188068678690",
+  appId: "1:188068678690:web:4ef24bffba97ba44b98e7b",
+};
 
-async function getEarthquakes(){
-    let res = await fetch(`https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson`);
-    let data = await  res.json();
+firebase.initializeApp(firebaseConfig);
 
-    const terremotos = data.features.map( t=> {
+const db = firebase.firestore(); 
 
-        return {
-            titulo: t.properties.title,
-            fecha: new Date(t.properties.time),
-            ubicacion: t.properties.place,
-            codigo: t.properties.code,
-            magnitud: t.properties.mag,
-            tipoMedida: t.properties.magType,
-            longitud: t.geometry.coordinates[0],
-            latitud: t.geometry.coordinates[1]
-        }
-    });
-    console.log(terremotos); 
-    return terremotos;
+
+async function getEarthquakes() {
+  let res = await fetch(
+    `https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson`,
+  );
+  let data = await res.json();
+
+  const terremotos = data.features.map((t) => {
+    return {
+      titulo: t.properties.title,
+      fecha: new Date(t.properties.time),
+      ubicacion: t.properties.place,
+      codigo: t.properties.code,
+      magnitud: t.properties.mag,
+      tipoMedida: t.properties.magType,
+      longitud: t.geometry.coordinates[0],
+      latitud: t.geometry.coordinates[1],
+    };
+  });
+  console.log(terremotos);
+  return terremotos;
 }
 
 async function initMap() {
@@ -33,41 +47,68 @@ async function initMap() {
 
   const terremotos = await getEarthquakes();
 
+  
   terremotos.forEach((t) => {
+    let article = document.createElement("article");
+    article.className = "favoritos";
+
+    let titulo = document.createElement("h2");
+    titulo.textContent = t.titulo;
+    article.appendChild(titulo);
+
+    let fecha = document.createElement("p");
+    fecha.textContent = `Fecha: ${t.fecha}`;
+    article.appendChild(fecha);
+
+    let ubicacion = document.createElement("p");
+    ubicacion.textContent = `Ubicación: ${t.ubicacion}`;
+    article.appendChild(ubicacion);
+
+    let codigo = document.createElement("p");
+    codigo.textContent = `Código: ${t.codigo}`;
+    article.appendChild(codigo);
+
+    let magnitud = document.createElement("p");
+    magnitud.textContent = `Magnitud: ${t.magnitud}`;
+    article.appendChild(magnitud);
+
+    let tipoMedida = document.createElement("p");
+    tipoMedida.textContent = `Tipo de medida:${t.tipoMedida}`;
+    article.appendChild(tipoMedida);
+
+    let addFavoritos = document.createElement("button");
+    addFavoritos.textContent = "Añadir a favoritos";
+    addFavoritos.className = "btn-add-favoritos";
+    article.appendChild(addFavoritos);
+
     L.circleMarker([t.latitud, t.longitud], {
       color: getColor(t.magnitud),
       fillColor: getColor(t.magnitud),
       fillOpacity: 0.5,
       radius: 5,
     })
-      .bindPopup(`
-        <article>
-          <h2>${t.titulo}</h2>
-          <p>Fecha: ${t.fecha}</p>
-          <p>Ubicación: ${t.ubicacion}</p>
-          <p>Código: ${t.codigo}</p>
-          <p>Magnitud: ${t.magnitud}</p>
-          <p>Tipo de medida:${t.tipoMedida}</p>
-        </article>`
-      ).addTo(map);
+      .bindPopup(article)
+      .addTo(map);
   });
 }
 initMap();
 
-function getColor(magnitud){
-    if(magnitud >= 6) return "red";
-    else if(magnitud >= 5) return "orange";
-    else if(magnitud >= 3) return "yellow";
-    else if(magnitud >= 1) return "green";
-    else return "white";
+function getColor(magnitud) {
+  if (magnitud >= 6) return "red";
+  else if (magnitud >= 5) return "orange";
+  else if (magnitud >= 3) return "yellow";
+  else if (magnitud >= 1) return "green";
+  else return "white";
 }
 
 /* mapa dos */
 let map2;
-async function fetchEarthquakesFiltrados(magnitud,fechaInicio,fechaFin){
-  let res = await fetch(`https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&minmagnitude=${magnitud}&starttime=${fechaInicio}&endtime=${fechaFin}`);
+async function fetchEarthquakesFiltrados(magnitud, fechaInicio, fechaFin) {
+  let res = await fetch(
+    `https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&minmagnitude=${magnitud}&starttime=${fechaInicio}&endtime=${fechaFin}`,
+  );
   let data = await res.json();
-  let filtroMagnitud = data.features.map(f => {
+  let filtroMagnitud = data.features.map((f) => {
     return {
       titulo: f.properties.title,
       fecha: new Date(f.properties.time),
@@ -78,7 +119,7 @@ async function fetchEarthquakesFiltrados(magnitud,fechaInicio,fechaFin){
       longitud: f.geometry.coordinates[0],
       latitud: f.geometry.coordinates[1],
     };
-  })
+  });
   return filtroMagnitud;
 }
 
@@ -102,11 +143,11 @@ async function buscarTerremotos() {
   /* validación magnitud */
 
   let mensaje = document.getElementById("mensaje");
-  mensaje.textContent ="";
+  mensaje.textContent = "";
   let errorMensaje = "";
   const regexMagnitud = /^[0-7](\.\d+)?$/;
-  
-  if(!regexMagnitud.test(magnitud)){
+
+  if (!regexMagnitud.test(magnitud)) {
     errorMensaje += "- La magnitud debe estar entre 0 y 7\n";
   }
 
@@ -115,13 +156,17 @@ async function buscarTerremotos() {
   const fechaInicioDate = new Date(fechaInicio);
   const fechaFinDate = new Date(fechaFin);
 
-  if (fechaInicioDate > fechaActual)errorMensaje += "- La fecha inicio no puede ser mayor que la fecha actual.\n";
-  if (fechaInicioDate > fechaFinDate) errorMensaje += "- La fecha inicio no puede ser mayor que la fecha fin.\n";
-  if(fechaFinDate > fechaActual) errorMensaje += "- La fecha fin no puede ser mayor que la fecha actual.\n";
-  
-  if (errorMensaje ) {
+  if (fechaInicioDate > fechaActual)
+    errorMensaje +=
+      "- La fecha inicio no puede ser mayor que la fecha actual.\n";
+  if (fechaInicioDate > fechaFinDate)
+    errorMensaje += "- La fecha inicio no puede ser mayor que la fecha fin.\n";
+  if (fechaFinDate > fechaActual)
+    errorMensaje += "- La fecha fin no puede ser mayor que la fecha actual.\n";
+
+  if (errorMensaje) {
     mensaje.textContent = errorMensaje;
-  }else{
+  } else {
     let filtros = await fetchEarthquakesFiltrados(
       magnitud,
       fechaInicio,
@@ -135,7 +180,8 @@ async function buscarTerremotos() {
         fillOpacity: 0.5,
         radius: 5,
       })
-        .bindPopup(`
+        .bindPopup(
+          `
         <article>
           <h2>${f.titulo}</h2>
           <p>Fecha: ${f.fecha}</p>
@@ -143,10 +189,13 @@ async function buscarTerremotos() {
           <p>Código: ${f.codigo}</p>
           <p>Magnitud: ${f.magnitud}</p>
           <p>Tipo de medida:${f.tipoMedida}</p>
-        </article>`
-        ).addTo(map2);
+        </article>`,
+        )
+        .addTo(map2);
     });
   }
 }
 initMap2();
-document.querySelector(".btn-buscar").addEventListener("click", () => buscarTerremotos());
+document
+  .querySelector(".btn-buscar")
+  .addEventListener("click", () => buscarTerremotos());
